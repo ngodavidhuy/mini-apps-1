fff9c4b4a241bb434398b7630cf5841a06509760
David Ngo <ngo.david.huy@gmail.com>
2018-12-10 11:16:04 -0800

    Lays out general structure of game
    Additionally, sets up event listeners to table cells

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,23 @@
+let cells = document.querySelectorAll('td');
+let playerTurn = true;
 
+console.log(Array.prototype.slice.call(cells).map( cell => cell.innerHTML));
 
+function checkForWin (cells) {
+  // let arr = cells.map( cell => cell.target.textContent);
+  // console.log(arr);
+}
+
+for (let cell of cells) {
+  cell.addEventListener('click', (evt) => {
+    console.log(evt.target.id);
+    console.log(evt.target.textContent);
+
+    if (!evt.target.textContent) {
+      evt.target.textContent = playerTurn ? 'X' : 'O';
+      playerTurn = !playerTurn;
+    }
+  });
+}
+
+// checkForWin(cells);
