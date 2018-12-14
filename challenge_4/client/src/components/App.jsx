c43517413b106c52e1b7097bb63ae4b3fb8b9e64
David Ngo <ngo.david.huy@gmail.com>
2018-12-14 00:44:29 -0800

    Implements basic requirements w/o winning logic tests

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
     this.state = {
       win: false,
       tie: false,
-      turn: 1,
+      player: 1,
       board: [
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
@@ -22,14 +22,14 @@ class App extends React.Component {
     this.handleMove = this.handleMove.bind(this);
   }
 
-  checkForWin(player) {
-    let board = this.state.board;
+  checkForWin(player, board) {
     let win = false;
 
     //VERTICAL
     for (let c = 0; c < 7; c++) {
       for (let r = 0; r < 3; r++) {
-        if (board[r+3][c] === player && board[r+2][c] === player && board[r+1][c] === player  && board[r][c] === player) {
+        if (board[r+3][c] === player && board[r+2][c] === player && 
+          board[r+1][c] === player  && board[r][c] === player) {
           win = true;
         }
       }
@@ -38,23 +38,37 @@ class App extends React.Component {
     //HORIZONTAL
     for (let c = 0; c < 4; c++) {
       for (let r = 0; r < 6; r++) {
-        if (board[r][c] === player && board[r][c+1] === player && board[r][c+2] === player && board[r][c+3] === player) {
+        if (board[r][c] === player && board[r][c+1] === player && 
+          board[r][c+2] === player && board[r][c+3] === player) {
           win = true;
         }
       }
     }
 
-    //MAJOR DIAGONAL
+    //LEFT-SIDE DIAGONALS
+    for (let c = 3; c < 7; c++) {
+      for (let r = 0; r < 3; r++) {
+        if (board[r][c] === player && board[r+1][c-1] === player && 
+          board[r+2][c-2] === player && board[r+3][c-3] === player) {
+          win = true;
+        }
+      }
+    }
 
-    //MINOR DIAGONAL
+    //RIGHT DIAGONALS
+    for (let c = 3; c < 7; c++) {
+      for (let r = 3; r < 6; r++) {
+        if (board[r][c] === player && board[r-1][c-1] === player && 
+          board[r-2][c-2] === player && board[r-3][c-3] === player) {
+          win = true;
+        }
+      }
+    }
 
-
-    console.log('check win');
-    console.log(win);
+    return win;
   }
 
-  checkForTie() {
-    let board = this.state.board;
+  checkForTie(board) {
     let spacesTakenCount = 0;
 
     for (let r = board.length - 1; r >= 0; r--) {
@@ -65,45 +79,45 @@ class App extends React.Component {
       }
     }
 
-    if (spacesTakenCount === 42) {
-      this.setState({
-        tie: true
-      }, () => {
-        console.log('GAME IS A TIE');
-        // console.log(this.state.tie);
-      });
-    } else {
-      // console.log('GAME ON!')
-    }
+    return spacesTakenCount === 42 ? true : false;
   }
 
   handleMove(evt) {
     let col = evt.target.dataset.y;
     let board = this.state.board.slice();
-    let currentPlayer = this.state.turn === 1 ? 1 : 2;
+    let currentPlayer = this.state.player === 1 ? 1 : 2;
     let nextPlayer = currentPlayer === 1 ? 2 : 1;
     let spaceFound = false;
+    let tie = this.state.tie;
+    let win = this.state.win;
 
     for (let r = board.length - 1; r >= 0; r--) {
         if (board[r][col] === 0 && !spaceFound) {
-          board[r][col] = (this.state.turn === 1 ? 1 : 2);
-          this.setState({
-            board: board,
-            turn: nextPlayer
-          }, () => {
-            this.checkForWin(currentPlayer);
-            this.checkForTie();
-          });
+          board[r][col] = (this.state.player === 1 ? 1 : 2);
+          win = this.checkForWin(currentPlayer, board);
+          tie = this.checkForTie(board);
           spaceFound = true;
         }
     }
+
+    this.setState({
+      win,
+      tie,
+      player: nextPlayer,
+      board
+    });
   }
 
   render() {
+
+    let prevPlayer = this.state.player === 1 ? 2 : 1;
+
     return (
       <div>
       <h1>Connect 4!</h1>
         <Board board={this.state.board} cols={7} rows={6} handleMove={this.handleMove} />
+        <p>{this.state.win ? `Player ${prevPlayer} Win!` : `Player 1 : Red // Player 2 : Blue`}</p>
+        <p>{this.state.tie ? `TIE GAME` : ``}</p>
       </div>
     );
   }
